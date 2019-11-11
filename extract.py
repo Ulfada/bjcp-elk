928a919bbc5329b5e4c1a9d36f0e0fc902093234
Benoit Delbosc <bdelbosc@nuxeo.com>
2019-11-11 13:36:14 +0100

    Enable to extract gravity as SG

--- extract.py
+++ extract.py
@@ -2,8 +2,13 @@
 # -*- coding: utf-8 -*-
 import json
 
-def dump(id, sub):
-    sub['style'] = id
+# If you want the gravity in Specific Gravity instead of Plato degree
+USE_SG = False
+BJCP_GUIDE_JSON = 'bjcp-2015.json'
+
+
+def dump(sid, sub):
+    sub['style'] = sid
     sub_id = sub['id']
     if sub_id[0].isdigit() and sub_id[1].isalpha():
         sub['subcategory'] = "0" + sub['id'] + " " + sub['name']
@@ -12,40 +17,55 @@ def dump(id, sub):
     if sub.get('vital_statistics'):
         calc_avg(sub, 'original_extract', 'og')
         calc_avg(sub, 'terminal_extract', 'fg')
+        if USE_SG:
+            convert_to_sg(sub, 'og')
+            convert_to_sg(sub, 'fg')
         calc_avg(sub, 'alcohol', 'abv')
         calc_avg(sub, 'bitterness', 'ibu')
         calc_avg(sub, 'color', 'srm')
-        sub['vital'] = "SRM: {}-{}, IBU: {}-{}, OG: {}-{}, FG: {}-{}, ABV: {}-{}".format(sub["srm"]["min"], sub["srm"]["max"], sub["ibu"]["min"], sub["ibu"]["max"], sub["og"]["min"], sub["og"]["max"], sub["fg"]["min"], sub["fg"]["max"], sub["abv"]["min"], sub["abv"]["max"])
+        sub['vital'] = "SRM: {:,}-{:,}, IBU: {:,}-{:,}, OG: {:,}-{:,}, FG: {:,}-{:,}, ABV: {:,}-{:,}".format(
+            sub["srm"]["min"], sub["srm"]["max"], sub["ibu"]["min"], sub["ibu"]["max"], sub["og"]["min"],
+            sub["og"]["max"], sub["fg"]["min"], sub["fg"]["max"], sub["abv"]["min"], sub["abv"]["max"])
         del sub['vital_statistics']
     if sub.get('color_classifications'):
         del sub['color_classifications']
     if sub.get('strength_classifications'):
         del sub['strength_classifications']
     if sub.get('styles'):
-        for subsub in sub['styles']:
-            dump(id, subsub)
+        for sub_sub in sub['styles']:
+            dump(sid, sub_sub)
     else:
         print('{"index":{}}')
         print(json.dumps(sub))
 
+
 def calc_avg(sub, item, rename):
     sub[item] = sub['vital_statistics'][item]
-    sub[item]['avg'] = float(format( (float(sub[item]['max']) + float(sub[item]['min']))/2, '.1f'))
+    sub[item]['avg'] = float(format((float(sub[item]['max']) + float(sub[item]['min'])) / 2, '.1f'))
     sub[rename] = sub[item]
     del sub[item]
 
-def parse():
-    with open('bjcp-2015.json') as json_file:
+
+def convert_to_sg(sub, item):
+    sub[item] = {'min': plato_to_sg(sub[item]['min']), 'max': plato_to_sg(sub[item]['max']),
+                 'avg': plato_to_sg(sub[item]['avg'])}
+
+
+def plato_to_sg(plato):
+    return int(round(1000 * (1 + (plato / (258.6 - ((plato / 258.2) * 227.1))))))
+
+
+def parse(file):
+    with open(file) as json_file:
         data = json.load(json_file)
         for style in data:
             try:
-                id = format(int(style['id']), '02d')
+                sid = format(int(style['id']), '02d')
             except ValueError:
-                id = style['id']
-            #print(id + ' Name: ' + style['name'])
+                sid = style['id']
+            # print(id + ' Name: ' + style['name'])
             for sub in style['subcategories']:
-                dump(id, sub)
+                dump(sid, sub)
 
 
-parse()
-
+parse(BJCP_GUIDE_JSON)
